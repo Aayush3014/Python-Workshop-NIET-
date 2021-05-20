2a4e6354de50b15db5df8b9779fde6b8b8398a11
Axxcelera <0201csai036@niet.co.in>
2021-05-20 20:35:54 +0530

    commit

---
+++ DAY 1/Program_6.py
@@ -0,0 +1,7 @@
+#String slicing
+string = "Welcome to the python workshop"
+lta = string[28:]
+fta = string[0:4]
+print(string)
+print(f"Last two alphabets : {lta}")
+print(f"First four alphabets : {fta}")
