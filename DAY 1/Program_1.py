c15c6848cd371e2338333552b39f0e9b6cdcb5de
Axxcelera <0201csai036@niet.co.in>
2021-05-20 20:00:06 +0530

    commit

---
+++ DAY 1/Program_1.py
@@ -0,0 +1,6 @@
+#Printing Name,Age,Branch.
+name = input("Enter your name  : ")
+age = int(input("Enter your age : "))
+branch = input("Enter your branch  : ")
+
+print(f'Name : {name} \nAge : {age} \nBranch : {branch} ')
