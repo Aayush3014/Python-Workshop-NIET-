042dfef595629d5119d6830ec6a2ce741a46e35f
Axxcelera <0201csai036@niet.co.in>
2021-05-20 20:09:57 +0530

    commit

---
+++ DAY 1/Program_4.py
@@ -0,0 +1,7 @@
+#WAP to calculate all the money.
+ten = int(input("Enter the number of 10rs coin : "))
+five = int(input("Enter the number of 5rs coin : "))
+two = int(input("Enter the number of 2rs coin : "))
+one = int(input("Enter the number of 1rs coin : "))
+total_money = ten*10 + five*5 + two*2 + one*1
+print(f"Total money in the piggy bank is : {total_money}")
